2f7a4c8266015d4e549dc6dae08291241dbed1ef
ty-mark <42213021+ty-mark@users.noreply.github.com>
2018-08-11 12:30:48 -0500

    Create hasCycle.java

---
+++ hasCycle.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode runner = head;
+        ListNode walker = head;
+        while (runner.next != null && runner.next.next != null) {
+            runner = runner.next.next;
+            walker = walker.next;
+            if (runner == walker) return true;
+        }
+        return false;
+    }
+}
