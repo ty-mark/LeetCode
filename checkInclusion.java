da034d3dca5bae87207a812e12a85d3f5f6c4760
ty-mark <42213021+ty-mark@users.noreply.github.com>
2018-08-15 23:13:53 -0500

    Update checkInclusion.java

--- checkInclusion.java
+++ checkInclusion.java
@@ -1,3 +1,9 @@
+// Given two strings s1 and s2, write a function to return true 
+// if s2 contains the permutation of s1. 
+// In other words, one of the first string's permutations is the substring of the second string.
+
+
+
 // Dumb algorithm that uses sort in while loop
 
 class Solution {
@@ -32,5 +38,33 @@ class Solution {
             }
         }
         return false;
+    }
+}
+
+// Sliding window with two pointers algorithm
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int len1 = s1.length();
+        int len2 = s2.length();
+        if (len1 > len2) return false;
+        int[] count = new int[26];
+        for (int i = 0; i < len1; i++) {
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+        if (allZeros(count)) return true;
+        for (int i = len1; i < len2; i++) {
+            count[s2.charAt(i) - 'a']--;
+            count[s2.charAt(i - len1) - 'a']++;
+            if (allZeros(count)) return true;
+        }
+        return false;
+    }
+    
+    private boolean allZeros(int[] count) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
+        }
+        return true;
     }
 }
