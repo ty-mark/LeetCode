5e61cf7ac330b1f973ae8c2538322183efd27367
ty-mark <42213021+ty-mark@users.noreply.github.com>
2018-08-16 22:39:49 -0500

    Update numSubarrayProductLessThanK.java

--- numSubarrayProductLessThanK.java
+++ numSubarrayProductLessThanK.java
@@ -23,3 +23,19 @@ class Solution {
         return count;
     }
 }
+
+// Brilliant two pointers algorithm
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k == 0) return 0;
+        int prod = 1, count = 0;
+        for (int left = 0, right = 0; right < nums.length; right++) {
+            prod *= nums[right];
+            while (prod >= k && left <= right) {
+                prod /= nums[left++];
+            }
+            count += right - left + 1; // think about this!!
+        }
+        return count;
+    }
+}
