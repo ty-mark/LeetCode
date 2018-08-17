e10a8b04060bf3b70822f7ff56402e296e814517
ty-mark <42213021+ty-mark@users.noreply.github.com>
2018-08-16 23:43:18 -0500

    Update partitionLabels.java

--- partitionLabels.java
+++ partitionLabels.java
@@ -25,3 +25,25 @@ class Solution {
         return ans;
     }
 }
+
+// use an array to store the last appearance of each character
+// 金钩钓鱼
+// fast method
+class Solution {
+    public List<Integer> partitionLabels(String S) {
+        List<Integer> ans = new ArrayList<>();
+        int[] end = new int[26];
+        for (int i = 0; i < S.length(); i++) {
+            end[S.charAt(i) - 'a'] = i;
+        }
+        int rightend = 0;
+        for (int left = 0, right = 0; right < S.length(); right++) {
+            rightend = Math.max(rightend, end[S.charAt(right) - 'a']);
+            if (right == rightend) {
+                ans.add(right - left + 1);
+                left = right + 1;
+            }
+        }
+        return ans;
+    }
+}
