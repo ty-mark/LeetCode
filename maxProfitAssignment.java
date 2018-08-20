951f4109f88762febf643fb92a1bfdca5de702ce
ty-mark <42213021+ty-mark@users.noreply.github.com>
2018-08-19 22:25:41 -0500

    Update maxProfitAssignment.java

--- maxProfitAssignment.java
+++ maxProfitAssignment.java
@@ -1,3 +1,14 @@
+/*
+We have jobs: difficulty[i] is the difficulty of the ith job, and profit[i] is the profit of the ith job. 
+Now we have some workers. worker[i] is the ability of the ith worker, which means that 
+this worker can only complete a job with difficulty at most worker[i]. 
+Every worker can be assigned at most one job, but one job can be completed multiple times.
+For example, if 3 people attempt the same job that pays $1, then the total profit will be $3.
+If a worker cannot complete any job, his profit is $0.
+What is the most profit we can make?
+*/
+
+
 // A smart way！！
 
 class Solution {
@@ -26,7 +37,7 @@ class Solution {
 
 // Two pointer with collections of pairs
 class Solution {
- public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
         List<Pair<Integer, Integer>> jobs = new ArrayList<>();
         int N = profit.length, res = 0, i = 0, maxp = 0;
         for (int j = 0; j < N; ++j) jobs.add(new Pair<Integer, Integer>(difficulty[j], profit[j]));
@@ -39,3 +50,4 @@ class Solution {
         }
         return res;
     }
+}
