cf176ddaac25afb172fba7e401984ab6d3dfe179
ty-mark <42213021+ty-mark@users.noreply.github.com>
2018-09-01 22:37:00 -0500

    Update mergeTwoLists.java

--- mergeTwoLists.java
+++ mergeTwoLists.java
@@ -6,6 +6,21 @@ Input: 1->2->4, 1->3->4
 Output: 1->1->2->3->4->4
 */
 
+// Recursive solution
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l2.next, l1);
+            return l2;
+        }
+    }
+}
+
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode ans = new ListNode(0);
