ded7c5af63e3b0687210977879b7e16289867afb
ty-mark <42213021+ty-mark@users.noreply.github.com>
2018-09-22 20:40:43 -0500

    Update reverseBetween.java

--- reverseBetween.java
+++ reverseBetween.java
@@ -1,3 +1,11 @@
+/*
+Reverse a linked list from position m to n. Do it in one-pass.
+Note: 1 ≤ m ≤ n ≤ length of list.
+Example:
+Input: 1->2->3->4->5->NULL, m = 2, n = 4
+Output: 1->4->3->2->5->NULL
+*/
+
 class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
         if (head == null || head.next == null || m == n) return head;
