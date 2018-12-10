e84756549032a8fcf44b06465ea217356e61ecbc
ty-mark <thyang@utexas.edu>
2018-12-09 21:15:21 -0600

    correct a bug

--- DP/minCut.java
+++ DP/minCut.java
@@ -14,12 +14,13 @@ class Solution {
 		char[] arr = s.toCharArray();
 		int n = arr.length;
 		if (n == 0 || n == 1) return 0;
+		// cut[i] --> cut[j] + 1 if s[j,i] is parlindrome
 		int[] cut = new int[n];
 		boolean[][] dp = new int[n][n];
 		for (int i = 0; i < n; i++) {
+			// the max possible # of cuts at the i-th position (no parlindrome)
+			int temp = i;
 			for (int j = 0; j <= i; j++) {
-				// the initial value set to be the max possible value
-				int temp = i;
 				// check if substring from j to i is parlindrome
 				if (arr[i] == arr[j] && (j + 1 > i - 1 || dp[i - 1][j + 1])) {
 					dp[i][j] = true;
@@ -27,6 +28,7 @@ class Solution {
 					temp = (j == 0) ? 0 : Math.min(temp, cut[j - 1] + 1);
 				} 
 			}
+			cut[i] = temp;
 		}
 		return cut[n - 1];
 	}
