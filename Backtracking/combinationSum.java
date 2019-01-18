c68de24a7897c71ebeeaaba69546c526ab136074
ty-mark <thyang@utexas.edu>
2019-01-17 21:31:23 -0600

    find all unique combinations summing to target with each number used multiple or only once by backtracking

--- Backtracking/combinationSum.java
+++ Backtracking/combinationSum.java
@@ -43,3 +43,41 @@ class Solution {
         }
     }
 }
+
+/*follow up:
+Each number in candidates may only be used once in the combination.
+
+Example 1:
+Input: candidates = [10,1,2,7,6,1,5], target = 8,
+A solution set is:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]*/
+
+// explaination: sorted array is [1,1,2,5,6,7,10]
+// without skipping duplicates, will obtain [1,2,5] twice in the list
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(list, new ArrayList<>(), candidates, target, 0);
+        return list;
+    }
+    private void backtrack(List<List<Integer>> list, List<Integer> subList, int[] nums, int target, int start) {
+        if (target < 0) return;
+        else if (target == 0) {
+            list.add(new ArrayList<>(subList));
+        } else {
+            for (int i = start; i < nums.length; i++) {
+                // skip the duplicate entries
+                if(i > start && nums[i] == nums[i-1]) continue;
+                subList.add(nums[i]);
+                backtrack(list, subList, nums, target - nums[i], i + 1);
+                subList.remove(subList.size() - 1);
+            }
+        }
+    }
+}
