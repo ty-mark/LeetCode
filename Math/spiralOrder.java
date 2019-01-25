816fd11c1d295cd91656b57e6b0d9ab3b665acc8
ty-mark <thyang@utexas.edu>
2019-01-24 20:56:26 -0600

    two spiral problems

--- Math/spiralOrder.java
+++ Math/spiralOrder.java
@@ -57,3 +57,31 @@ class Solution {
         return list;
     }
 }
+
+// Given n, return a n*n matrix with numbers in spiral order from 1 to n^2
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int num = 1;
+        int rowBegin = 0, rowEnd = n - 1, colBegin = 0, colEnd = n - 1;
+        while (rowBegin <= rowEnd && colBegin <= colEnd) {
+            for (int i = colBegin; i <= colEnd; i++) {
+                matrix[rowBegin][i] = num++;
+            }
+            rowBegin++;
+            for (int i = rowBegin; i <= rowEnd; i++) {
+                matrix[i][colEnd] = num++;
+            }
+            colEnd--;
+            for (int i = colEnd; i >= colBegin; i--) {
+                matrix[rowEnd][i] = num++;
+            }
+            rowEnd--;
+            for (int i = rowEnd; i >= rowBegin; i--) {
+                matrix[i][colBegin] = num++;
+            }
+            colBegin++;
+        }
+        return matrix;
+    }
+}
