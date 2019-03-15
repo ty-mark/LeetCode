0d6d7fd3c933917f8c5c368045e9e1ab1cc51413
ty-mark <thyang@utexas.edu>
2019-03-14 21:52:08 -0500

    update the iterative solution

--- LinkedList/mergeTwoLists.java
+++ LinkedList/mergeTwoLists.java
@@ -21,6 +21,31 @@ class Solution {
     }
 }
 
+// iterative solution
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                prev = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                prev = l2;
+                l2 = l2.next;
+            }
+        }
+        if (l1 == null) prev.next = l2;
+        if (l2 == null) prev.next = l1;
+        return dummy.next;
+    }
+}
+
+// deprecated
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode ans = new ListNode(0);
