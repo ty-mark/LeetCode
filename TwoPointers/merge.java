3cb8469824fc1b35ed8ea34d85ff8261700d6989
ty-mark <thyang@utexas.edu>
2019-03-16 00:16:38 -0500

    merge two sorted arrays in place from back to front

--- TwoPointers/merge.java
+++ TwoPointers/merge.java
@@ -8,6 +8,24 @@
 // nums2 = [2,5,6],       n = 3
 // Output: [1,2,2,3,5,6]
 
+// from back to front, in place
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int right = m + n - 1, p1 = m - 1, p2 = n - 1;
+        while (p1 >= 0 || p2 >= 0) {
+            if (p1 < 0) nums1[right--] = nums2[p2--];
+            else if (p2 < 0) break; // no need to put elements to nums1, which are already there
+            else if (nums1[p1] <= nums2[p2]) {
+                nums1[right--] = nums2[p2--];
+            } else if (nums1[p1] > nums2[p2]) {
+                nums1[right--] = nums1[p1--];
+            }
+            
+        }
+    }
+}
+
+// from front to back, with aux array
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = 0, j = 0;
